9e92545af9dbad6e5fb5c555b701314efb7daf88
Thomas Valhøj <thomas.valhoj@gmail.com>
2026-02-22 12:21:06 +0100

    Hardcoded redirect url

--- js/config.js
+++ js/config.js
@@ -8,7 +8,7 @@ export const CONFIG = {
   oauthClientId: "3b89b95c-d658-463e-9280-30a5bd7f4c2c",
 
   // Keep simple for hash routing; must match exactly in Genesys app config
-  oauthRedirectUri: window.location.origin + window.location.pathname,
+  oauthRedirectUri: "https://proud-pebble-04fa37b03.2.azurestaticapps.net",
 
   // Add scopes as needed (start minimal)
   oauthScopes: ["openid"],
