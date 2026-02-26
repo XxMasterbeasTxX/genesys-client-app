800181b3e98b1a0b141002a8124b8981bb66baf5
Thomas Valhøj <thomas.valhoj@gmail.com>
2026-02-26 09:10:01 +0100

    Supervisor Test

--- js/pages/data-tables/dataTablesConfig.js
+++ js/pages/data-tables/dataTablesConfig.js
@@ -15,7 +15,7 @@
  * When false (Administrator mode), all non-key fields are editable
  * (subject to the user's API permissions).
  */
-export const SUPERVISOR_MODE = false;
+export const SUPERVISOR_MODE = true;
 
 /**
  * Per-table configuration.
